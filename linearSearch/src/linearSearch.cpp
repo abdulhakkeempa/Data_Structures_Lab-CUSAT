e5b29e6cc514d8eb8080fba12926e5d89f3ddd5e
hakkeempa <abdulhakkeempa2002@gmail.com>
2022-06-17 22:12:10 +0530

    linear search initial

---
+++ linearSearch/src/linearSearch.cpp
@@ -0,0 +1,28 @@
+//c++ code to implement linear search
+#include <iostream>
+using namespace std;
+
+int linearSearch(int data[],int size,int search_key){
+	for (int i = 0; i < size; ++i) {
+		cout<<data[i]<<endl;
+		if (data[i]==search_key){
+			return i;
+		}
+	}
+	return -1;
+}
+
+
+int main() {
+	int data [] = {1,2,3,4,5,6,2,7};
+	//extracting the size of the array
+	int size = sizeof(data)/sizeof(int);
+	int pos = linearSearch(data,size,10);
+	if (pos!=-1){
+		cout<<"Element found at "<<pos+1<<" position."<<endl;
+	}
+	else{
+		cout<<"Element not Present"<<endl;
+	}
+	return 0;
+}
