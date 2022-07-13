8e8a4fe523fe0fd0fa6f06e4b43842453ab480af
hakkeempa <abdulhakkeempa2002@gmail.com>
2022-07-13 16:21:01 +0530

    initial version merge sort

---
+++ mergeSort/src/mergeSort.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int* data,int length);
+void merge(int* array,int begin,int mid,int end){
+	int i,j,k;
+	int sortedArray[end];
+	i = begin;
+	j = mid+1;
+	k = begin;
+	while (i<=mid and j<=end){
+		if (array[i]<=array[j]){
+			sortedArray[k] = array[i];
+			i++;
+		}
+		else{
+			sortedArray[k] = array[j];
+			j++;
+		}
+		k++;
+	}
+	if(i>mid){
+		while(j<=end){
+			sortedArray[k] = array[j];
+			j++;
+			k++;
+		}
+	}
+	else{
+		while(i<=mid){
+			sortedArray[k] = array[i];
+			i++;
+			k++;
+		}
+	}
+	for (int var = begin; var <= end; ++var) {
+		array[var] = sortedArray[var];
+	}
+}
+
+
+void mergeSort(int* array,int begin,int end){
+	if(begin==end){
+		return;
+	}
+	int mid = (begin+end)/2;
+	mergeSort(array, begin, mid);
+	mergeSort(array, mid+1, end);
+	merge(array,begin,mid,end);
+}
+
+
+void printArray(int* data,int length){
+	for (int var = 0; var < length; ++var) {
+		cout<<data[var]<<" ";
+		cout<<endl;
+	}
+	cout<<endl;
+}
+
+int main() {
+	int array[] = {60,50,72,22,3,4,5};
+	int length = sizeof(array)/sizeof(array[0]);
+	mergeSort(array, 0,length);
+	for (int var = 0; var <= length; ++var) {
+		cout<<array[var]<<" ";
+	}
+	return 0;
+}
