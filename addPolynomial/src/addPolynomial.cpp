8617129d387b9aa37d402d5aba687b1cbcfdb79f
hakkeempa <abdulhakkeempa2002@gmail.com>
2022-09-10 16:50:40 +0530

    polynomial addition menu oriented

--- addPolynomial/src/addPolynomial.cpp
+++ addPolynomial/src/addPolynomial.cpp
@@ -53,11 +53,32 @@ void printPolynomial(int* polynomial,int degree){
 	    }
 	}
 	cout<<polynomial[var];
+	cout<<endl;
 }
 
 int main() {
-	int array1[] = {5,4,10,11,25};
-	int array2[] = {25,4,10,11,25};
-	addPolynomial(array2,array1, 4,4);
+	int degree1,degree2;
+	cout<<"Welcome"<<endl;
+	cout<<"Enter the degree of first polynomial"<<endl;
+	cin>>degree1;
+	int *poly1 = new int[degree1+1];
+	for (int var = degree1; var >=0;var--) {
+		cout<<"Coefficient of x"<<var<<" : ";
+		cin>>poly1[var];
+	}
+	cout<<"Polynomial 1 : ";
+	printPolynomial(poly1, degree1);
+	cout<<"Enter the degree of second polynomial"<<endl;
+	cin>>degree2;
+	int *poly2 = new int[degree2+1];
+	for (int var = degree2; var >=0;var--) {
+		cout<<"Coefficient of x"<<var<<" : ";
+		cin>>poly2[var];
+	}
+	cout<<"Polynomial 2 : ";
+	printPolynomial(poly2, degree2);
+	cout<<"Sum : ";
+	addPolynomial(poly1, poly2, degree1, degree2);
+	cout<<"Succesfully Exited!"<<endl;
 	return 0;
 }
