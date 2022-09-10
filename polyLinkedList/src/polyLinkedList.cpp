c807d9bf04fdf5789ed3892db2f7889331cb4fdc
hakkeempa <abdulhakkeempa2002@gmail.com>
2022-09-10 17:32:48 +0530

    polynomial linkedlist menu oriented

--- polyLinkedList/src/polyLinkedList.cpp
+++ polyLinkedList/src/polyLinkedList.cpp
@@ -98,22 +98,52 @@ void LinkedList::addPolynomial(LinkedList poly1,LinkedList poly2){
 
 int main() {
 	LinkedList poly1;
-	poly1.createNode(9, 4);
-	poly1.createNode(19, 3);
-	poly1.createNode(9, 2);
-	poly1.createNode(2, 1);
-	poly1.createNode(2, 0);
-	poly1.printPolynomial();
-
 	LinkedList poly2;
-	poly2.createNode(25, 5);
-	poly2.createNode(9, 4);
-	poly2.createNode(19, 3);
-	poly2.createNode(19, 0);
-	poly2.printPolynomial();
-
-	LinkedList sum;
-	sum.addPolynomial(poly1, poly2);
-	sum.printPolynomial();
+	int mainOption,loopOption=1;
+	int listOption = 1;
+	int coef,pow;
+	while (loopOption==1){
+		listOption = 1;
+		cout<<"1.Input Polynomial 1 \n2.Input Polynomial 2\n3. Sum of Polynomial"<<endl;
+		cin>>mainOption;
+		switch (mainOption) {
+			case 1:
+				while(listOption==1){
+					cout<<"Enter the coefficient"<<endl;
+					cin>>coef;
+					cout<<"Enter the power"<<endl;
+					cin>>pow;
+					poly1.createNode(coef, pow);
+					cout<<"Press 0 to quit\n1 to continue"<<endl;
+					cin>>listOption;
+				}
+				break;
+			case 2:
+				while(listOption==1){
+					cout<<"Enter the coefficient"<<endl;
+					cin>>coef;
+					cout<<"Enter the power"<<endl;
+					cin>>pow;
+					poly2.createNode(coef, pow);
+					cout<<"press 0 to quit"<<endl;
+					cin>>listOption;
+				}
+				break;
+			case 3:
+			{
+				LinkedList sum;
+				sum.addPolynomial(poly1, poly2);
+				cout<<"Sum : ";
+				sum.printPolynomial();
+				break;
+			}
+			default:
+				cout<<"Invalid Statement"<<endl;
+				break;
+		}
+		cout<<"Do you want to continue\n1.Continue\n2.Quit"<<endl;
+		cin>>loopOption;
+	}
+	cout<<"Successfully Exited!"<<endl;
 	return 0;
 }
